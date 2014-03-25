f0a8272420979cddcbd53bb681bf15c7684f048b
Paweł Baran <PwelBrn@gmail.com>
2014-03-25 01:49:36 +0100

    form in controller, validation

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,11 +1,25 @@
 package controllers
 
 import play.api.mvc._
+import play.api.data.Forms
+import play.api.data.Form
+import models.MatchingInput
 
 object Application extends Controller {
 
+  val matchingForm = Form(
+    Forms.mapping(
+      "pattern" -> Forms.nonEmptyText,
+      "text" -> Forms.nonEmptyText
+    )(MatchingInput.apply)(MatchingInput.unapply))
+
   def index = Action {
-    Ok(views.html.index())
+    Ok(views.html.index)
   }
 
+  def doMatch = Action {
+    implicit request => matchingForm.bindFromRequest.fold(
+      _ => BadRequest(views.html.index),
+      matchingInput => Redirect(routes.Application.index))
+  }
 }
---
+++ app/models/MatchingInput.scala
@@ -0,0 +1,3 @@
+package models
+
+case class MatchingInput(pattern: String, text: String)
